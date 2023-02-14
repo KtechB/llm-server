da5e0abae655eeaa7428424aee4c126bd1795a79
ktechb <k_techevo@yahoo.co.jp>
2023-02-14 16:14:05 +0900

    add llm

--- src/llm_server/main.py
+++ src/llm_server/main.py
@@ -1,13 +1,40 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, Field
+
+from llm_server.simple_agent import ask_question
 
 app = FastAPI()
 
+origins = ["http://localhost:5173", "http://localhost:8000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
+
+class Message(BaseModel):
+    text: str = Field(title="Request message to LLM.", max_length=1000)
+
+
+class LLMResponse(BaseModel):
+    text: str
+
+
+@app.get("/healthcheck")
 def read_root():
-    return {"Hello": "World"}
+    return {}
+
+
+@app.post("/")
+async def run_dummy_llm(message: Message) -> LLMResponse:
+    answer = ask_question(message.text)
+    return LLMResponse(text=answer)
 
 
 @app.get("/items/{item_id}")
---
+++ src/llm_server/simple_agent.py
@@ -0,0 +1,16 @@
+from typing import Literal
+from langchain import PromptTemplate, LLMChain
+from langchain.llms import OpenAI
+
+
+def ask_question(question: str) -> str:
+    llm = OpenAI(temperature=0.9)
+    template = """Question: {question}
+
+    Answer: Let's think step by step."""
+
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+    answer = llm_chain.run(question)
+    return answer
